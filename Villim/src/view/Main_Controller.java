ff7acd71a7989235e8deb75b0bf75233968d9cde
wjddmsry050921 <82300939+wjddmsry050921@users.noreply.github.com>
2021-11-04 20:00:38 +0900

    회원가입 이름 걸러내기 하는 중
    실행해서 회원가입 중 이름에 ㄱ~ㅣ 넣으면 알림창 계속 뜸 조심

--- Villim/src/view/Main_Controller.java
+++ Villim/src/view/Main_Controller.java
@@ -9,6 +9,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -31,12 +33,13 @@ public class Main_Controller {
     PreparedStatement pstmt = null;
     String sql = "";
 	Connection con = JDBCUtill.getConnection();
+	Alert alert = new Alert(AlertType.WARNING);
+	
 	public void join() {
 		try {
 			
 			String joinId = join_id.getText();
 			String joinPass = join_pass.getText();
-			String joinName = join_name.getText();
 			String joinEmail = join_email.getText();
 			int tree = 0;
 			
@@ -58,9 +61,39 @@ public class Main_Controller {
 			if(joinPass != null) {
 				pstmt.setString(2, joinPass);
 			}
-			if(joinName != null) {
-				pstmt.setString(3, joinName);
-			}
+//			if(joinName != null && name_cnt == joinName.length() && joinName.length() <= 8) {
+//				pstmt.setString(3, joinName);
+//			} else {
+//			alert.setTitle("메세지");
+//			alert.setHeaderText(null);
+//			alert.setContentText("이름을 다시 지어주세요.");
+//			alert.showAndWait();
+//			}
+//			while(true) {
+//				작명 기준에 적합한지 확인할 때 사용하는 변수
+//				int name_cnt = 0;
+			
+//				(조인 네임 입력 다시 받아야함)
+			
+//				작명 기준에 적합한지 확인
+//				String joinName = join_name.getText();
+//				for(byte i = 0; i < joinName.length(); i++) {
+//					if((int)(joinName.charAt(i)) >= 12593 && (int)(joinName.charAt(i)) <= 12643) {
+//					} else {
+//						name_cnt += 1;
+//					}
+//				}
+//				if(joinName != null && name_cnt == joinName.length() && joinName.length() <= 8) {
+//					pstmt.setString(3, joinName);
+//					break;
+//				} else {
+//					alert.setTitle("메세지");
+//					alert.setHeaderText(null);
+//					alert.setContentText("이름을 다시 지어주세요.");
+//					alert.showAndWait();
+//					continue;
+//				}
+//			}
 			if(joinEmail != null) {
 				pstmt.setString(4, joinEmail);
 			}
