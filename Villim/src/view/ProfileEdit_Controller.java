b6f76abf3aa41dfd931dd15ce19cbc675ed401e4
jeongayoung323 <monayoung0323@naver.com>
2021-11-15 23:29:48 +0900

    프로젝트 수정

--- Villim/src/view/ProfileEdit_Controller.java
+++ Villim/src/view/ProfileEdit_Controller.java
@@ -3,9 +3,11 @@ package view;
 import database.JDBCUtill;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
+import util.MethodUtil;
 import util.Singleton;
 
 import java.net.URL;
@@ -17,97 +19,110 @@ import java.util.ResourceBundle;
 import static view.Login_Controller.userId;
 
 public class ProfileEdit_Controller implements Initializable {
-        //이 클래스가 실행되면 호출되는 메소드                   ^ 이거 있어야함 ^
-        @Override
-        public void initialize(URL location, ResourceBundle resources) {
-            //현재 화면의 이전 화면을 변수
-            Singleton.getInstance().setPreviousLayoutClass(Singleton.getInstance().getCLC());
-            //현재 화면을 나타내는 변수
-            Singleton.getInstance().setCLC(getClass().getSimpleName());
+	// 이 클래스가 실행되면 호출되는 메소드 ^ 이거 있어야함 ^
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// 현재 화면의 이전 화면을 변수
+		Singleton.getInstance().setPreviousLayoutClass(Singleton.getInstance().getCLC());
+		// 현재 화면을 나타내는 변수
+		Singleton.getInstance().setCLC(getClass().getSimpleName());
 
-        }
-        // 알림창
-        @FXML
-        public Pane alertPane;
-        @FXML
-        public Text alertText;
-        public boolean isAliveThread;
+	}
 
-        public void alert(String text) {
-            Thread t = new Thread() {
-                public void run() {
-                    try {
-                        isAliveThread = true;
-                        Thread.sleep(500);
-                        for (int i = -15; i <= 0; i++) {
-                            alertPane.setLayoutY(i * 4);
-                            Thread.sleep(20);
-                        }
-                        Thread.sleep(2000);
-                        for (int i = 0; i >= -15; i--) {
-                            alertPane.setLayoutY(i * 4);
-                            Thread.sleep(20);
-                        }
-                        isAliveThread = false;
-                    } catch (Exception e) {
-                        System.out.println(getClass().getName() + " 쓰레드 오류: " + e);
-                    }
+	// 알림창
+	@FXML
+	public Pane alertPane;
+	@FXML
+	public Text alertText;
+	public boolean isAliveThread;
 
-                }
-            };
-            alertText.setText(text);
-            if (isAliveThread) {
-                return;
-            }
-            t.start();
-        }
-        ResultSet rs = null;
-        PreparedStatement pstmt = null;
-        String sql = "";
-        Connection conn = JDBCUtill.getInstance().getConnection();
+	public void alert(String text) {
+		Thread t = new Thread() {
+			public void run() {
+				try {
+					isAliveThread = true;
+					Thread.sleep(500);
+					for (int i = -15; i <= 0; i++) {
+						alertPane.setLayoutY(i * 4);
+						Thread.sleep(20);
+					}
+					Thread.sleep(2000);
+					for (int i = 0; i >= -15; i--) {
+						alertPane.setLayoutY(i * 4);
+						Thread.sleep(20);
+					}
+					isAliveThread = false;
+				} catch (Exception e) {
+					System.out.println(getClass().getName() + " 쓰레드 오류: " + e);
+				}
 
-        // 닉네임 변경
-        @FXML
-        private TextField memberNickName;
+			}
+		};
+		alertText.setText(text);
+		if (isAliveThread) {
+			return;
+		}
+		t.start();
+	}
 
-        public void nickNameChange() {
+	MethodUtil methodUtil = new MethodUtil();
+	
+	ResultSet rs = null;
+	PreparedStatement pstmt = null;
+	String sql = "";
+	Connection conn = JDBCUtill.getInstance().getConnection();
 
-            String NickName = memberNickName.getText();
-            sql = "update users set name='" + NickName + "' WHERE id='" + Singleton.getInstance().getAccountId() + "'";
+	// 닉네임 변경
+	@FXML
+	private TextField memberNickName;
 
+	public void nickNameChange() {
 
-            try {
-                pstmt = conn.prepareStatement(sql);
-                pstmt.executeUpdate();
-                // 변경된 회원정보를 새로고침 하려면?
-                alert("변경에 성공했습니다");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    // 비밀번호 변경
-    @FXML
-    private TextField MemberPassword;
-    @FXML
-    private TextField MemberPasswordCheck;
+		String NickName = memberNickName.getText();
+		sql = "update users set name='" + NickName + "' WHERE id='" + Singleton.getInstance().getAccountId() + "'";
 
-    public void passwordChange() {
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.executeUpdate();
+			// 변경된 회원정보를 새로고침 하려면?
+			alert("변경에 성공했습니다");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        String password = MemberPassword.getText();
-        String passwordCheck = MemberPasswordCheck.getText();
-        sql = "update users set password='" + password + "' WHERE id='" + userId + "'";
+	// 비밀번호 변경
+	@FXML
+	private TextField MemberPassword;
+	@FXML
+	private TextField MemberPasswordCheck;
 
-        try {
-            if (passwordCheck.equals(password)) {
-                pstmt = conn.prepareStatement(sql);
-                pstmt.executeUpdate();
-                // 변경된 회원정보를 새로고침 하려면?
-                alert("변경에 성공했습니다");
-            } else {
-                alert("비밀번호가 일치하지 않습니다");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public void passwordChange() {
+
+		String password = MemberPassword.getText();
+		String passwordCheck = MemberPasswordCheck.getText();
+		sql = "update users set password='" + password + "' WHERE id='" + userId + "'";
+
+		try {
+			if (passwordCheck.equals(password)) {
+				pstmt = conn.prepareStatement(sql);
+				pstmt.executeUpdate();
+				// 변경된 회원정보를 새로고침 하려면?
+				alert("변경에 성공했습니다");
+			} else {
+				alert("비밀번호가 일치하지 않습니다");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	// 프로필 화면 전환
+	@FXML
+	private Button changeProfileBtn;
+
+	public void changeProfile() {
+		methodUtil.changeScene("/view/Profile_Layout.fxml", changeProfileBtn);
+	}
+	
 }
