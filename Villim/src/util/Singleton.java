27fe34fd77805c717391f3e484a89f160a767f42
JavaPSG <92962048+JavaPSG@users.noreply.github.com>
2021-11-26 10:51:15 +0900

    l.

--- Villim/src/util/Singleton.java
+++ Villim/src/util/Singleton.java
@@ -1,6 +1,8 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Singleton {
@@ -24,6 +26,7 @@ public class Singleton {
 	private String accountNick;
 	private String now2;
 	private boolean writeSuccess;
+
 	public boolean isWriteSuccess() {
 		return writeSuccess;
 	}
@@ -68,8 +71,19 @@ public class Singleton {
 	public void setCurrentCategory(String currentCategory) {
 		this.currentCategory = currentCategory;
 	}
+
 	public void debug(String t) {
-			System.out.println("[ 디버그 ] " + t);
+		System.out.println("[ 디버그 ] " + t);
 	}
+	
+	private List<String> postList = new ArrayList<String>();
+	public void setPostList(List<String> t) {
+		Collections.sort(t, Collections.reverseOrder());
+		this.postList = postList;
+	}
+	public List<String> getPostList(){
+		return this.postList;
+	}
+
 
 }
--- Villim/src/view/Home_Controller.java
+++ Villim/src/view/Home_Controller.java
@@ -1,13 +1,23 @@
 package view;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import database.JDBCUtill;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Pagination;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import util.MethodUtil;
@@ -15,6 +25,7 @@ import util.Singleton;
 
 public class Home_Controller implements Initializable {
 	// 이 클래스가 실행되면 호출되는 메소드 ^ 이거 있어야함 ^
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		if (Singleton.getInstance().sceneList.get(Singleton.getInstance().sceneList.size() - 1)
@@ -25,15 +36,96 @@ public class Home_Controller implements Initializable {
 			alert("게시물을 업로드하였습니다");
 			Singleton.getInstance().setWriteSuccess(false);
 		}
-		
+		batchPost();
 		// postListPage.set
 
 	}
-	// private final ChangeListener<Number> paginationChangeListener = (observable,
-	// oldValue, newValue) -> changePage();
+
+	@FXML
+	public Pane postPane1;
+	@FXML
+	public Pane postPane2;
+	@FXML
+	public Pane postPane3;
+	@FXML
+	public Pane postPane4;
+	
+	@FXML
+	public Text recommand2;
+	@FXML
+	public Text title2;
+	@FXML
+	public Text category2;
+	@FXML
+	public Text writer2;
+	@FXML
+	public Text time2;
+	@FXML
+	public ImageView image2;
+	
+	@FXML
+	public Text recommand3;
+	@FXML
+	public Text title3;
+	@FXML
+	public Text category3;
+	@FXML
+	public Text writer3;
+	@FXML
+	public Text time3;
+	@FXML
+	public ImageView image3;
+	
+	@FXML
+	public Text recommand4;
+	@FXML
+	public Text title4;
+	@FXML
+	public Text category4;
+	@FXML
+	public Text writer4;
+	@FXML
+	public Text time4;
+	@FXML
+	public ImageView image4;
+
+	public void batchPost() {
+		List<String> postList = new ArrayList<String>();
+		String a = "";
+		try {
+			Connection conn = JDBCUtill.getInstance().getConnection();
+			String sql = "SELECT id, title, content, category, now, recommand, code FROM resource";
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			while (rs.next()) {
+				System.out.println(rs.getString("id"));
+				a = rs.getString("title");
+				System.out.println(rs.getString("content"));
+				System.out.println(rs.getString("category"));
+				System.out.println(rs.getString("now"));
+				System.out.println(rs.getInt("recommand"));
+				postList.add(rs.getString("code"));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		Singleton.getInstance().setPostList(postList);
+		if (a.length() >= 10) {
+			System.out.println(a.substring(0, 10) + "...");
+		}
+		else {
+			System.out.println(a);
+		}
+	}
+	// private final ChangeListener<Number> paginationChangeListener =
+	// (observable,oldValue, newValue) -> changePage();
 
 	@FXML
 	public Pagination postListPage;
+
+	// private void changePage() { label.setText(String.format("Current Page:
+	// %d",postListPage.getCurrentPageIndex())); }
+
 	// 알림창
 	@FXML
 	public Pane alertPane;
